b22deaf8609a50681073af2108cdf9e9acfd72cf
Anshu Singh <anshumnitian@gmail.com>
2020-10-16 12:57:09 +0530

    preview option added

--- android/src/main/java/com/imagepicker/activity/CameraActivity.java
+++ android/src/main/java/com/imagepicker/activity/CameraActivity.java
@@ -2,12 +2,16 @@ package com.imagepicker.activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
 
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
@@ -17,19 +21,15 @@ import com.otaliastudios.cameraview.CameraListener;
 import com.otaliastudios.cameraview.CameraView;
 import com.otaliastudios.cameraview.PictureResult;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.URI;
-import com.imagepicker.R;
+
 
 public class CameraActivity extends AppCompatActivity {
 
     CameraView cameraView;
 
     ProgressDialog progressDialog;
+
+    String imageSavePath
 
 
 
@@ -45,32 +45,23 @@ public class CameraActivity extends AppCompatActivity {
 
         cameraView.setLifecycleOwner(this);
 
-
+        imageSavePath=getIntent().getStringExtra("path");
 
         cameraView.addCameraListener(new CameraListener() {
             @Override
             public void onPictureTaken(@NonNull PictureResult result) {
                 super.onPictureTaken(result);
 
-                final String imageSavePath=getIntent().getStringExtra("path");
-
-                Log.d("DATA", "onPictureTaken here: "+ (result.getData().length));
 
                 progressDialog.dismiss();
-                // the File to save , append increasing numeric counter to prevent files from getting overwritten.
-                Intent intent=new Intent();
-                intent.putExtra("path",imageSavePath);
-                try {
-                    FileOutputStream outputStream = new FileOutputStream(imageSavePath);
-                    outputStream.write(result.getData());
-                    outputStream.flush();
-                    outputStream.close();
-                    setResult(RESULT_OK,intent);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    setResult(RESULT_CANCELED,intent);
+                if (result.getData().length>0){
+                    //image captured
+                    showPreview(result.getData());
+                }else{
+                    //0 byte received
+                    setResult(RESULT_CANCELED);
+                    finish();
                 }
-                finish();
                 return;
             }
 
@@ -90,5 +81,50 @@ public class CameraActivity extends AppCompatActivity {
                 cameraView.takePictureSnapshot();
             }
         });
+    }
+
+
+    private void showPreview(byte[] data){
+
+        Bitmap bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
+
+        final Dialog dialog=new Dialog(this,android.R.style.Theme_Black_NoTitleBar_Fullscreen);
+        dialog.setContentView(R.layout.camera_image_preview_layout);
+
+        AppCompatImageView imgOk=dialog.findViewById(R.id.img_ok);
+        AppCompatImageView imgCancel=dialog.findViewById(R.id.img_cancel);
+        AppCompatImageView imgPreview=dialog.findViewById(R.id.img_preview);
+
+        imgPreview.setImageBitmap(bmp);
+
+        imgCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        imgOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //save the image and set result
+                // the File to save , append increasing numeric counter to prevent files from getting overwritten.
+                Intent intent=new Intent();
+                intent.putExtra("path",imageSavePath);
+                try {
+                    FileOutputStream outputStream = new FileOutputStream(imageSavePath);
+                    outputStream.write(result.getData());
+                    outputStream.flush();
+                    outputStream.close();
+                    setResult(RESULT_OK,intent);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    setResult(RESULT_CANCELED,intent);
+                }
+                finish();
+            }
+        });
+
+        dialog.show();
     }
 }
